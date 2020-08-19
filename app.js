b70a40756fce1b5ba3c7df7d8c7f743ddc086686
abquadir <abquadir73@yah00.com>
2020-08-19 16:41:33 +0530

    first commit

--- app.js
+++ app.js
@@ -6,48 +6,106 @@ const menu = [
       img: "./images/bike1.jpg",
   
     },
+  
     {
       id: 2,
-      category:"bikes",
-      img: "./images/bike2.jpg",
-      
+      category:"hills",
+      img: "./images/hill3.jpg",
     },
     {
       id: 3,
       category:"cars",
       img: "./images/car1.jpg",
     },
-      
     {
       id: 4,
-      category:"bikes",
-      img: "./images/bike3.jpg",
+      category:"hills",
+      img: "./images/hill5.jpg",
     },
-    {
-      id: 5,
-      category:"cars",
-      img: "./images/car2.jpg",
-    },
+      {
+        id:5,
+        category:"cars",
+        img:"./images/car2.jpg"
+      },
+ 
     {
       id: 6,
-      category:"bikes",
-      img: "./images/bike4.jpg",
-    },
+      category:"animals",
+      img: "./images/animal3.jpg",}
+   ,
     {
-        id: 4,
+        id: 7,
         category:"bikes",
         img: "./images/bike5.jpg",
       },
       {
-        id: 5,
+        id: 8,
         category:"cars",
-        img: "./images/car3.jpg",
+        img: "./images/car4.jpg",
       },
       {
-        id: 1,
+        id: 9,
         category:"cars",
         img: "./images/car5.jpg",
       },
+      {
+        id: 10,
+        category:"hills",
+        img: "./images/hill1.jpg",
+      },
+      {
+        id:11,
+        category:"animals",
+        img: "./images/animal1.jpg",
+      },
+      {
+        id: 12,
+        category:"hills",
+        img: "./images/hill5.jpg",
+      },
+      {
+        id: 13,
+        category:"bikes",
+        img: "./images/bike4.jpg",
+      },
+        {
+          id: 14,
+          category:"animals",
+          img: "./images/animal2.jpg",
+        },
+        {
+          id: 15,
+          category:"hills",
+          img: "./images/hill2.jpg",
+        },
+        {
+          id: 16,
+          category:"cars",
+          img: "./images/car3.jpg",
+        }
+        ,
+        {
+          id: 17,
+          category:"bikes",
+          img: "./images/bike2.jpg",
+          
+        },
+        {
+          id: 18,
+          category:"animals",
+          img: "./images/animal4.jpg",
+        },
+        {
+            id: 19,
+            category:"animals",
+            img: "./images/animal5.jpg",
+          },
+          {
+            id: 20,
+            category:"bikes",
+            img: "./images/bike3.jpg",
+          },
+       
   ];
 
   const container = document.querySelector(".container")
